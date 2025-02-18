b3a00030252cfb93b4c2e534c744df386fb00d7f
Force <maxfunmaxfun672@gmail.com>
2025-02-19 01:25:05 +0300

    update files via auto commit

--- MatProg/Lab_1/Lab_1/Main.cpp
+++ MatProg/Lab_1/Lab_1/Main.cpp
@@ -24,22 +24,26 @@ int _tmain(int argc, _TCHAR* argv[])
 	setlocale(LC_ALL, "rus");
 
 	auxil::start();                          // старт генерации 
-	t1 = clock();                            // фиксация времени 
-	for (int i = 0; i < CYCLE; i++)
+
+	for (size_t i = 0; i < 10; i++)
 	{
-		av1 += (double)auxil::iget(-100, 100); // сумма случайных чисел 
-		av2 += auxil::dget(-100, 100);         // сумма случайных чисел 
+		int cycles = pow(10, i);
+		t1 = clock();                            // фиксация времени 
+		for (int i = 0; i < cycles; i++)
+		{
+			av1 += (double)auxil::iget(-100, 100); // сумма случайных чисел 
+			av2 += auxil::dget(-100, 100);         // сумма случайных чисел 
+		}
+		t2 = clock();                            // фиксация времени 
+
+		std::cout << std::endl << "количество циклов:         " << cycles;
+		std::cout << std::endl << "среднее значение (int):    " << av1 / cycles;
+		std::cout << std::endl << "среднее значение (double): " << av2 / cycles;
+		std::cout << std::endl << "продолжительность (у.е):   " << (t2 - t1);
+		std::cout << std::endl << "                  (сек):   "
+			<< ((double)(t2 - t1)) / ((double)CLOCKS_PER_SEC);
+		std::cout << std::endl;
 	}
-	t2 = clock();                            // фиксация времени 
-
-
-	std::cout << std::endl << "количество циклов:         " << CYCLE;
-	std::cout << std::endl << "среднее значение (int):    " << av1 / CYCLE;
-	std::cout << std::endl << "среднее значение (double): " << av2 / CYCLE;
-	std::cout << std::endl << "продолжительность (у.е):   " << (t2 - t1);
-	std::cout << std::endl << "                  (сек):   "
-		<< ((double)(t2 - t1)) / ((double)CLOCKS_PER_SEC);
-	std::cout << std::endl;
 
 	t1 = 0, t2 = 0;
 
