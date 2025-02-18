9ddce88897541ed9473bc9c93cb9f03c8b3ae704
Force <maxfunmaxfun672@gmail.com>
2025-02-18 15:22:41 +0300

    update files via auto commit

--- TPI/1/ASPA/ASPA001/Program.cs
+++ TPI/1/ASPA/ASPA001/Program.cs
@@ -1,9 +1,25 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddHttpLogging(logging =>
+        {
+            logging.LoggingFields = HttpLoggingFields.All;
+            logging.RequestHeaders.Add("sec-ch-ua");
+            logging.ResponseHeaders.Add("MyResponseHeader");
+            logging.MediaTypeOptions.AddText("application/javascript");
+            logging.RequestBodyLogLimit = 4096;
+            logging.ResponseBodyLogLimit = 4096;
+            logging.CombineLogs = true;
+        });
+
         var app = builder.Build();
+
+        app.UseHttpLogging();
 
         app.MapGet("/", () => "Hello World!");
 
