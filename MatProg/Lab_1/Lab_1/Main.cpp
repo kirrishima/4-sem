ab16dc94346347f5fa5ed386dcfb0a245497e699
Force <maxfunmaxfun672@gmail.com>
2025-02-19 00:55:01 +0300

    update files via auto commit

--- MatProg/Lab_1/Lab_1/Main.cpp
+++ MatProg/Lab_1/Lab_1/Main.cpp
@@ -5,56 +5,18 @@
 #include <locale>
 #include <tchar.h>
 
-#define  CYCLE  10                       // количество циклов  
+#define  CYCLE  1000000                       // количество циклов  
 
-ull fibonachi(ull n)
+ull fibonacci(ull n)
 {
-	ull a = 0, b = 1;
-	switch (n)
-	{
-	case 1:
-		return 0;
-	case 2:
-		return 1;
-	default:
-		for (size_t i = 0; i < n - 2; i++)
-		{
-			b = a + b;
-			a = b - a;
-		}
-		return b;
-	}
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	setlocale(LC_ALL, "rus");
-
-	for (size_t i = 1; i <= 10; i++)
-	{
-		double  av1 = 0, av2 = 0;
-		clock_t  t1 = 0, t2 = 0;
-
-		auxil::start();                          // старт генерации 
-		t1 = clock();                            // фиксация времени 
-		int cycles = pow(10, i + 2) / 2;
-		for (int i = 0; i < cycles; i++)
-		{
-			av1 += (double)auxil::iget(-100, 100); // сумма случайных чисел 
-			av2 += auxil::dget(-100, 100);         // сумма случайных чисел 
-		}
-		t2 = clock();                            // фиксация времени 
-
-
-		std::cout << std::endl << "количество циклов:         " << cycles;
-		std::cout << std::endl << "среднее значение (int):    " << av1 / cycles;
-		std::cout << std::endl << "среднее значение (double): " << av2 / cycles;
-		std::cout << std::endl << "продолжительность (у.е):   " << (t2 - t1);
-		std::cout << std::endl << "                  (сек):   "
-			<< ((double)(t2 - t1)) / ((double)CLOCKS_PER_SEC);
-		std::cout << std::endl;
-	}
-
 
 	double  av1 = 0, av2 = 0;
 	clock_t  t1 = 0, t2 = 0;
@@ -62,16 +24,35 @@ int _tmain(int argc, _TCHAR* argv[])
 	setlocale(LC_ALL, "rus");
 
 	auxil::start();                          // старт генерации 
-
-	for (size_t i = 1; i <= CYCLE; i++)
+	t1 = clock();                            // фиксация времени 
+	for (int i = 0; i < CYCLE; i++)
 	{
-		ull x = powl(10, i);
+		av1 += (double)auxil::iget(-100, 100); // сумма случайных чисел 
+		av2 += auxil::dget(-100, 100);         // сумма случайных чисел 
+	}
+	t2 = clock();                            // фиксация времени 
 
-		std::cout << std::endl << "число фибоначи номер " << x;
-		std::cout << std::endl << "цикл " << i;
+
+	std::cout << std::endl << "количество циклов:         " << CYCLE;
+	std::cout << std::endl << "среднее значение (int):    " << av1 / CYCLE;
+	std::cout << std::endl << "среднее значение (double): " << av2 / CYCLE;
+	std::cout << std::endl << "продолжительность (у.е):   " << (t2 - t1);
+	std::cout << std::endl << "                  (сек):   "
+		<< ((double)(t2 - t1)) / ((double)CLOCKS_PER_SEC);
+	std::cout << std::endl;
+
+	t1 = 0, t2 = 0;
+
+	auxil::start();                          // старт генерации 
+
+	for (size_t i = 1; i <= 10; i++)
+	{
+		ull x = 10 * i / 2;
+
+		std::cout << std::endl << '(' << "цикл " << i << ") число фибоначи номер " << x;
 
 		t1 = clock();
-		fibonachi(x);
+		fibonacci(x);
 		t2 = clock();
 
 		std::cout << std::endl << "продолжительность (у.е):   " << (t2 - t1);
